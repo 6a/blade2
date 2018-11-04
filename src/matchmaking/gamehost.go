d4f39da02bc10ff48e2991612329f13d23e03484
James Stanton <jestanton@outlook.com>
2018-11-04 16:08:50 +0900

    Fixed gamehost bug where goroutine uses 100% cpu time if a game exists

--- src/matchmaking/gamehost.go
+++ src/matchmaking/gamehost.go
@@ -11,6 +11,7 @@ import (
 )
 
 const idleTick = 2000 * time.Millisecond
+const gameTick = (1000 / 50) * time.Millisecond
 const queueSize = 2048
 
 var queueCount = tools.MaxInt(int(float32(runtime.NumCPU())*0.75), 1)
@@ -25,6 +26,7 @@ func InitGameHost() {
 	for index := 0; index < queueCount; index++ {
 		go func(index int) {
 			executed := false
+			currentGameIndex := 0
 			for {
 				select {
 				case game := <-gamePipelines[index]:
@@ -40,6 +42,13 @@ func InitGameHost() {
 
 							client.Drop(templates.Information{Code: e.OponentDroppedConnection, Message: ""})
 						}
+					}
+
+					currentGameIndex++
+					if len(gamePipelines[index]) >= currentGameIndex {
+						currentGameIndex = 0
+						time.Sleep(gameTick)
+						print("TICK\n")
 					}
 				default:
 					if !executed {
