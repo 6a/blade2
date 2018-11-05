6bdbc0df6df481c6dcb66a65244409567f639917
james_stanton <james.stanton@sony.com>
2018-11-05 12:41:28 +0900

    Replaced channel with slice  in gamehost

--- src/server/gamehost.go
+++ src/server/gamehost.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"log"
+	"math"
 	"runtime"
 	"time"
 
@@ -12,66 +13,62 @@ import (
 
 const idleTick = 2000 * time.Millisecond
 const gameTick = (1000 / 50) * time.Millisecond
-const queueSize = 2048
 
 var queueCount = tools.MaxInt(int(float32(runtime.NumCPU())*0.75), 1)
-var gamePipelines = make([]chan *Game, queueCount)
+var gamePipelines = make([][]*Game, queueCount)
 
 // InitGameHost initializes the game management queue
 func InitGameHost() {
-	for i := range gamePipelines {
-		gamePipelines[i] = make(chan *Game, queueSize)
-	}
+	// Initialize
+	// for i := range gamePipelines {
+	// 	gamePipelines[i] = []*Game{}
+	// }
 
-	for index := 0; index < queueCount; index++ {
-		go func(index int) {
-			executed := false
-			currentGameIndex := 0
+	// Indices:
+	// - pli (Pipeline Index)
+	// - gi (Game Index)
+	// - ci (Client Index)
+	for pli := 0; pli < queueCount; pli++ {
+		go func(pli int) {
 			for {
-				select {
-				case game := <-gamePipelines[index]:
-					if game.Client[0].IsAlive() && game.Client[1].IsAlive() {
-						executed = true
-						game.RelayUpdates()
-						gamePipelines[index] <- game
-					} else {
-						for _, client := range game.Client {
-							if !client.IsAlive() {
-								log.Printf("Client [%s] in game [%d] dropped connection", client.ID, game.ID)
+				iterations := len(gamePipelines[pli])
+				if iterations > 0 {
+					for gi := 0; gi < iterations; gi++ {
+						if gamePipelines[pli][gi].Client[0].IsAlive() && gamePipelines[pli][gi].Client[1].IsAlive() {
+							gamePipelines[pli][gi].RelayUpdates()
+						} else {
+							for ci := 1; ci > 0; ci-- {
+								if !gamePipelines[pli][gi].Client[ci].IsAlive() {
+									log.Printf("Client [%s] in game [%d] dropped connection", gamePipelines[pli][gi].Client[ci].ID, gamePipelines[pli][gi].ID)
+									gamePipelines[pli][gi].Client[ci].Drop(templates.Information{Code: e.Drop, Message: ""})
+									gamePipelines[pli][gi].Client[1-ci].Drop(templates.Information{Code: e.OponentDroppedConnection, Message: ""})
+								}
 							}
 
-							client.Drop(templates.Information{Code: e.OponentDroppedConnection, Message: ""})
+							gamePipelines[pli] = append(gamePipelines[pli][:gi], gamePipelines[pli][gi+1:]...)
 						}
 					}
 
-					currentGameIndex++
-					if len(gamePipelines[index]) >= currentGameIndex {
-						currentGameIndex = 0
-						time.Sleep(gameTick)
-					}
-				default:
-					if !executed {
-						time.Sleep(idleTick)
-					}
-
-					executed = false
+					time.Sleep(gameTick * 10)
+				} else {
+					time.Sleep(idleTick)
 				}
 			}
-		}(index)
+		}(pli)
 	}
 }
 
 // AddGame will add a pointer to a game object to the least populated game pipeline
 func AddGame(game *Game) {
-	lowestCount := queueSize
-	nextPipe := gamePipelines[0]
-	for _, pipeline := range gamePipelines {
+	lowestCount := math.MaxInt64
+	nextPipeIndex := 0
+	for ppi, pipeline := range gamePipelines {
 		l := len(pipeline) // This may be a bit of a bottleneck. If that is the case, then it would be efficient to keep track of pipe utilization
 		if l < lowestCount {
 			lowestCount = l
-			nextPipe = pipeline
+			nextPipeIndex = ppi
 		}
 	}
 
-	nextPipe <- game
+	gamePipelines[nextPipeIndex] = append(gamePipelines[nextPipeIndex], game)
 }
