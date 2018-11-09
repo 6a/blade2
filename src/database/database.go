f6c797293d0ad842b6c8dca243ca98045e884235
james_stanton <james.stanton@sony.com>
2018-11-09 12:13:17 +0900

    Added profile pic path and bio to player db, also removed sizes from all

--- src/database/database.go
+++ src/database/database.go
@@ -11,38 +11,40 @@ const databasePath = "../../db/"
 // Note: https://www.tutorialspoint.com/sqlite/sqlite_indexed_by.htm
 var schemas = [3]string{
 	`CREATE TABLE IF NOT EXISTS player (
-	player_id varchar(64) NOT NULL,
-	player_name varchar(16) NOT NULL UNIQUE,
-	email varchar(64) NOT NULL UNIQUE,
-	password_hash varchar(128) NOT NULL,
-	token varchar(64) NOT NULL,
-	token_expiry INT(32) NOT NULL DEFAULT '0',
+	player_id varchar NOT NULL,
+	player_name varchar NOT NULL UNIQUE,
+	profile_picture varchar NOT NULL,
+	bio varchar NOT NULL,
+	email varchar NOT NULL UNIQUE,
+	password_hash varchar NOT NULL,
+	token varchar NOT NULL,
+	token_expiry INT NOT NULL DEFAULT '0',
 	PRIMARY KEY (player_id)
 );
 CREATE INDEX player_name_idx ON player (player_name);
 CREATE INDEX email_idx ON player (email);`,
 
 	`CREATE TABLE IF NOT EXISTS rating (
-	player_id varchar(64) NOT NULL,
-	elo INT(16) NOT NULL DEFAULT '1200',
-	wins INT(16) NOT NULL DEFAULT '0',
-	losses INT(16) NOT NULL DEFAULT '0',
-	draws INT(16) NOT NULL DEFAULT '0',
+	player_id varchar NOT NULL,
+	elo INT NOT NULL DEFAULT '1200',
+	wins INT NOT NULL DEFAULT '0',
+	losses INT NOT NULL DEFAULT '0',
+	draws INT NOT NULL DEFAULT '0',
 	PRIMARY KEY (player_id)
 );`,
 
 	`CREATE TABLE IF NOT EXISTS game (
-	game_id INT(64) NOT NULL AUTO_INCREMENT DEFAULT '0',
-	player_id_1 varchar(64) NOT NULL,
-	player_id_2 varchar(64) NOT NULL,
-	player_1_elo_start INT(16) NOT NULL DEFAULT '1200',
-	player_1_elo_end INT(16) NOT NULL DEFAULT '1200',
-	player_2_elo_start INT(16) NOT NULL DEFAULT '1200',
-	player_2_elo_end INT(16) NOT NULL DEFAULT '1200',
-	game_end_time INT(32) NOT NULL DEFAULT '0',
-	winner INT(1) NOT NULL,
-	state_start varchar(64) NOT NULL,
-	actions varchar(256) NOT NULL,
+	game_id INT NOT NULL AUTO_INCREMENT DEFAULT '0',
+	player_id_1 varchar NOT NULL,
+	player_id_2 varchar NOT NULL,
+	player_1_elo_start INT NOT NULL DEFAULT '1200',
+	player_1_elo_end INT NOT NULL DEFAULT '1200',
+	player_2_elo_start INT NOT NULL DEFAULT '1200',
+	player_2_elo_end INT NOT NULL DEFAULT '1200',
+	game_end_time INT NOT NULL DEFAULT '0',
+	winner INT NOT NULL,
+	state_start varchar NOT NULL,
+	actions varchar NOT NULL,
 	PRIMARY KEY (match_id)
 );
 CREATE INDEX player_id_1_idx ON game (player_id_1);
